b92b2d063dc950d7f1e8b69dd958ab87ee3bf090
ryuichi-works <ryu1.works@gmail.com>
2023-10-31 11:42:23 +0000

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>ホームページ</h1>
+    <>
+      <h1>ホームページ</h1>
+      <h2>コンテナに入りました</h2>
+    </>
   )
 }
